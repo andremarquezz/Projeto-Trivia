8d721b618c1636dd245bba04bb8db7bc11585291
Andre Marquez <andre.marqquez@gmail.com>
2022-05-30 15:44:07 -0300

    feat: Adiciona testes da pagina de Feedbacks

--- src/tests/02-Feedbacks.test.js
+++ src/tests/02-Feedbacks.test.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { screen, fireEvent, render } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
-import App from '../App'
+import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 describe('Testa funcionalidade da tela Feedback', () => {
-    it('Testa se ao carregar a página a rota é feedback', () => {
-      const { history } = renderWithRouterAndRedux(<App />);
+  it('Testa se ao carregar a página a rota é feedback', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
 
-      history.push('/feedback');
+    history.push('/feedback');
 
-      expect(history.location.pathname).toBe('/feedback');
-    });
+    expect(history.location.pathname).toBe('/feedback');
+  });
 
   it('Verifica se renderiza todos os textos e botões corretos', () => {
     const { history } = renderWithRouterAndRedux(<App />);
@@ -19,10 +20,10 @@ describe('Testa funcionalidade da tela Feedback', () => {
     const textFeedback = screen.getByText(/Could be better.../i);
     expect(textFeedback).toBeInTheDocument();
 
-    const btnRank = screen.getByRole('button', { name: /Ranking/i});
+    const btnRank = screen.getByRole('button', { name: /Ranking/i });
     expect(btnRank).toBeInTheDocument();
 
-    const btnPlay = screen.getByRole('button', { name: /Play Again/i});
+    const btnPlay = screen.getByRole('button', { name: /Play Again/i });
     expect(btnPlay).toBeInTheDocument();
   });
 
@@ -30,20 +31,21 @@ describe('Testa funcionalidade da tela Feedback', () => {
     const { history } = renderWithRouterAndRedux(<App />);
     history.push('/feedback');
 
-    const btnPlay = screen.getByRole('button', { name: /Play Again/i});
+    const btnPlay = screen.getByRole('button', { name: /Play Again/i });
     fireEvent.click(btnPlay);
     expect(history.location.pathname).toBe('/');
   });
 
   it('Verifica se ao clicar no botão ranking é redirecionado para a página de ranking', () => {
     const { history } = renderWithRouterAndRedux(<App />);
+    const user = [{ name: 'teste', picture: 'teste' }];
+    localStorage.setItem('ranking', JSON.stringify(user));
     history.push('/feedback');
-
-    const btnRank = screen.getByRole('button', { name: /Ranking/i})
-    fireEvent.click(btnRank);
-    expect(history.location.pathname).toBe('/ranking');
+    const btnRank = screen.getByRole('button', { name: /Ranking/i });
+    expect(btnRank).toBeInTheDocument();
+    userEvent.click(btnRank);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/ranking');
   });
-
-  it('',() => {
-  });
+  // it('', () => {});
 });
