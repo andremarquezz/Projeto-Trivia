af3bf0451f4d32e3048458d74ab3a6a35774b1bc
Andre Marquez <andre.marqquez@gmail.com>
2022-06-01 00:50:21 -0300

    Updates

---
+++ precache-manifest.06e3a57a7cdaac30668a511216c1b1ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f0ca561d46138b5e2037abfc31216c",
+    "url": "/ProjetoTrivia/index.html"
+  },
+  {
+    "revision": "dec98fc86c7e91da0ee6",
+    "url": "/ProjetoTrivia/static/css/main.a07c7b2b.chunk.css"
+  },
+  {
+    "revision": "e4f991dfef00dbee04f9",
+    "url": "/ProjetoTrivia/static/js/2.6e8e7988.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/ProjetoTrivia/static/js/2.6e8e7988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec98fc86c7e91da0ee6",
+    "url": "/ProjetoTrivia/static/js/main.bfc448c9.chunk.js"
+  },
+  {
+    "revision": "4130bd7f7f80c5654087",
+    "url": "/ProjetoTrivia/static/js/runtime-main.ba7b48fe.js"
+  },
+  {
+    "revision": "5a597645b4ad4648aaf6eb5fe0c4776b",
+    "url": "/ProjetoTrivia/static/media/audio_background_game.5a597645.mp3"
+  },
+  {
+    "revision": "9272e1c537f2a8ac857d627ade79f5b6",
+    "url": "/ProjetoTrivia/static/media/settings.9272e1c5.png"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/ProjetoTrivia/static/media/trivia.466d153e.png"
+  }
+]);
