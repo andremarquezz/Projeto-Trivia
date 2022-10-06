d2ca3cfd3caff140411bcfe559302e84829b7bdc
Jey <andre.marqquez@gmail.com>
2022-10-06 15:23:36 -0300

    build: altera hashRouter para browserRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <HashRouter>
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
 
-  document.getElementById('root'),
+  document.getElementById('root')
 );
